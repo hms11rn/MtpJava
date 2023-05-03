a4d6e5b224071b7316c62a784d2ff80fc89cce42
hms11rn <hmselik@gmail.com>
2023-05-03 23:00:43 +0300

    Added ability to copy folder

--- MtpJava/src/main/java/com/github/hms11rn/mtp/win32/PortableDeviceContainerObjectWin32.java
+++ MtpJava/src/main/java/com/github/hms11rn/mtp/win32/PortableDeviceContainerObjectWin32.java
@@ -5,6 +5,7 @@ import com.github.hms11rn.mtp.content.PortableDeviceFolderObject;
 import com.github.hms11rn.mtp.content.PortableDeviceObject;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class PortableDeviceContainerObjectWin32 extends PortableDeviceObjectWin32 implements PortableDeviceContainerObject {
@@ -24,10 +25,32 @@ public class PortableDeviceContainerObjectWin32 extends PortableDeviceObjectWin3
         return new PortableDeviceObjectWin32(content.createObject(file, id), content);
     }
 
-
-
     @Override
     public PortableDeviceFolderObject createFolderObject(String name) {
         return new PortableDeviceFolderObjectWin32(content.createFolder(name, id), content);
     }
-}
+
+    @Override
+    public void copy(String path) {
+        File pathFile = new File(path);
+        if (!pathFile.exists())
+            throw new RuntimeException(new FileNotFoundException("File \"" + path +"\"" + "is not a valid file"));
+        copyFolder(path);
+    }
+
+        private void copyFolder(String path) {
+            PortableDeviceObject[] objects = getChildObjects();
+            for (PortableDeviceObject obj : objects) {
+                if (!(obj instanceof PortableDeviceContainerObject)) {
+                    content.copyFile(obj.getId(), path + "\\" + obj.getOriginalFileName());
+                } else {
+                    String name = obj.getOriginalFileName();
+                    File newDir = new File(path + "\\" + name);
+                    newDir.mkdir();
+                    obj.copy(newDir.getAbsolutePath());
+                }
+            }
+        }
+
+    }
+
